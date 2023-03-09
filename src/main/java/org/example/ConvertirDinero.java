1cda666fdbf033ebdb7685447d2d1f1fb61386ab
FerarersUnl <rayman30@live.com.mx>
2023-03-08 23:52:55 -0600

    added checkers

--- src/main/java/org/example/ConvertirDinero.java
+++ src/main/java/org/example/ConvertirDinero.java
@@ -10,6 +10,8 @@ public class ConvertirDinero extends JFrame {
     private JTextField textField;
     private JLabel Error;
     private JPanel PanelConvert;
+    private JComboBox opcionesBox;
+    private JFormattedTextField newValue;
 
     public ConvertirDinero() {
         setContentPane(PanelConvert);
@@ -24,7 +26,43 @@ public class ConvertirDinero extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    int value = Integer.parseInt(textField.getText());
+                    double value = Integer.parseInt(textField.getText());
+                    int choice = opcionesBox.getSelectedIndex();
+                    Integer finalValue;
+                    switch (choice) {
+                        case 0:
+                            // Convert to Dollars
+                            finalValue = (int) (value / 19.96);
+                            break;
+                        case 1:
+                            finalValue = (int) (value / 18.95);
+                            break;
+                        case 2:
+                            finalValue = (int) (value / 21.29);
+                            break;
+                        case 3:
+                            finalValue = (int) (value / 0.13);
+                            break;
+                        case 4:
+                            finalValue = (int) (value / 0.014);
+                            break;
+                        case 5:
+                            finalValue = (int) (value / 0.056);
+                            break;
+                        case 6:
+                            finalValue = (int) (value / 0.053);
+                            break;
+                        case 7:
+                            finalValue = (int) (value / 0.047);
+                            break;
+                        case 8:
+                            finalValue = (int) (value / 7.62);
+                            break;
+                        case 9:
+                            finalValue = (int) (value / 73.50);
+                            break;
+                    }
+
                 } catch (Exception exception) {
                     Error.setVisible(true);
                 }
